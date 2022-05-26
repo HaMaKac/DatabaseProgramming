4acc3d18a60690235c02d45b589d648b79b2c851
HaMaKac <50404753+HaMaKac@users.noreply.github.com>
2022-05-26 15:10:47 +0200

    Changed triggers_functions

--- triggers_functions.sql
+++ triggers_functions.sql
@@ -11,12 +11,70 @@ BEGIN
     FROM payment
     WHERE paymentID = :NEW.paymentID;
     
-    IF (TO_DATE(:NEW.end_date) - TO_DATE(:NEW.start_date)) >= 21 THEN
+    IF (:NEW.end_date - :NEW.start_date) >= 21 THEN
         UPDATE payment
         SET fee = v_new_fee - 5/100 * v_new_fee
         WHERE paymentID = :NEW.paymentID;
     END IF;
 END;
+
+-- 2
+CREATE OR REPLACE TRIGGER t_check_date
+    BEFORE INSERT ON RESERVATION
+    REFERENCING NEW AS NEW OLD AS OLD
+    FOR EACH ROW
+DECLARE
+    v_start_date DATE;
+    v_end_date DATE;
+    e_incorrect_date EXCEPTION;
+BEGIN
+    SELECT start_date, end_date 
+    INTO v_start_date, v_end_date
+    FROM reservation
+    WHERE reservationid = :NEW.reservationid;
+    
+    IF v_start_date > v_end_date THEN
+        RAISE e_incorrect_date;
+    END IF;
+        
+EXCEPTION
+    WHEN e_incorrect_date THEN
+        raise_application_error(-20000, 'End date cannot be earlier than start date');
+    WHEN OTHERS THEN
+        raise_application_error(-20000, 'Error');
+END;
+
+-- 3
+CREATE OR REPLACE TRIGGER t_check_capacity
+    BEFORE INSERT ON RESERVATION
+    REFERENCING NEW AS NEW OLD AS OLD
+    FOR EACH ROW
+DECLARE
+    v_capacity NUMBER;
+    e_wrong_count EXCEPTION;
+BEGIN
+    SELECT capacity INTO v_capacity
+    FROM room
+    WHERE roomid = :NEW.roomid;
+    
+    IF :NEW.people > v_capacity THEN
+        RAISE e_wrong_count;
+    END IF;
+EXCEPTION
+    WHEN e_wrong_count THEN
+        raise_application_error(-20000, 'Wrong count of guests');
+    WHEN OTHERS THEN
+        raise_application_error(-20000, 'Error');
+END;
+
+-- Trigger tests
+DELETE FROM RESERVATION
+WHERE reservationid = 10;
+
+INSERT INTO Reservation(reservationid, guestid, roomid, paymentid, start_date, end_date, people) 
+VALUES (10, 10, 25, 10, '03-JUN-22', '04-JUN-22', 2);
+
+SELECT * FROM Reservation;
 
 -- Functions
 -- 1
@@ -37,43 +95,58 @@ BEGIN
     ELSE
         RAISE e_incorrect_iso;
     END IF;
+    
 EXCEPTION
     WHEN e_incorrect_iso THEN
-        DBMS_OUTPUT.PUT_LINE('Error: Incorrect ISO');
+        DBMS_OUTPUT.PUT_LINE('Incorrect ISO');
+        RETURN -1;
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Error: An unexpected exception has occured');
+        DBMS_OUTPUT.PUT_LINE('Error');
+        RETURN -1;
+        
 END CONVERT_CURRENCY;
 
-SET SERVEROUTPUT ON;
 BEGIN
-    DBMS_OUTPUT.PUT_LINE(CONVERT_CURRENCY(100, 'EUR'));
-    DBMS_OUTPUT.PUT_LINE(CONVERT_CURRENCY(100, 'ISK'));
+	DBMS_OUTPUT.PUT_LINE('Results for 100 -> EUR: ' || CONVERT_CURRENCY(100, 'EUR'));
+	DBMS_OUTPUT.PUT_LINE('Results for 100 -> GBP: ' || CONVERT_CURRENCY(100, 'GBP'));
 END;
 
 -- 2
 CREATE OR REPLACE FUNCTION COUNT_DIALING_CODE(
-    p_dialing_code IN VARCHAR2)
+    p_dialing_code IN NVARCHAR2)
 RETURN NUMBER
 IS
-    v_count NUMBER;
+    v_count NUMBER := 0;
     
     CURSOR c_count IS 
         SELECT phone_number
         FROM guest
-        WHERE SUBSTR(phone_number, 0, LENGTH(p_dialing_code)) = p_dialing_code;
+        WHERE SUBSTR(phone_number, 1, LENGTH(p_dialing_code)) = p_dialing_code;
     
 BEGIN
     FOR elem IN c_count
     LOOP
-        IF LENGTH(phone_number) - LENGTH(p_dialing_code) = 9 THEN
-            v_count := v_count + 1;
-        END IF;
+        v_count := v_count + 1;
     END LOOP;
+    
+    RETURN v_count;
     
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('Error: No data found');
+        DBMS_OUTPUT.PUT_LINE('No data found');
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Error: An unexpected exception has occured');
+        DBMS_OUTPUT.PUT_LINE('Error');
     
 END COUNT_DIALING_CODE;
+
+--Function tests
+-- 1
+BEGIN
+	DBMS_OUTPUT.PUT_LINE('Results for 100 -> EUR: ' || CONVERT_CURRENCY(100, 'EUR'));
+	DBMS_OUTPUT.PUT_LINE('Results for 100 -> GBP: ' || CONVERT_CURRENCY(100, 'GBP'));
+END;
+-- 2
+BEGIN
+	DBMS_OUTPUT.PUT_LINE('Results for +48: ' || COUNT_DIALING_CODE('+48'));
+	DBMS_OUTPUT.PUT_LINE('Results for +42: ' || COUNT_DIALING_CODE('+42'));
+END;
